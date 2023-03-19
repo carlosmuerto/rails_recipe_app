bdcce4d6a4c988bd54fddf555f4307e03eae5dc6
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-03-18 21:34:57 -0400

    Fix minor lint

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,9 @@
-import * as React from "react";
-export const Button = () => {
+// import * as React from "react";
+
+const Button = () => {
   return <button>Boop</button>;
-};
+}
+
+export {
+  Button
+}
