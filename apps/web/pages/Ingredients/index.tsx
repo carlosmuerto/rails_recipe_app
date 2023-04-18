1e783d40dc8d10d0cbfeab0793ca44efa9c644cc
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-04-17 21:48:38 -0400

    Add route to add food to form

--- apps/web/pages/Ingredients/index.tsx
+++ apps/web/pages/Ingredients/index.tsx
@@ -1,11 +1,8 @@
-import { useRouter } from 'next/router';
 import Layout from 'ui/Base/Layout';
 import * as Ingredients from 'ui/Ingredients';
 
 
 const IndexIngredients = () => {
-  const router = useRouter()
-  const { pid } = router.query
   return (
     <Layout>
       <Ingredients.Show.Table />
---
+++ apps/web/pages/Ingredients/new/index.tsx
@@ -0,0 +1,13 @@
+import Layout from 'ui/Base/Layout';
+import * as Ingredients from 'ui/Ingredients';
+
+
+const NewIngredient = () => {
+  return (
+    <Layout>
+      <Ingredients.Add.Form />
+    </Layout>
+  );
+}
+
+export default NewIngredient; 
