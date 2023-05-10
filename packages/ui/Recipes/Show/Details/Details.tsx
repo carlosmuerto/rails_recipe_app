ef9f5028243dfe60db8dcd7ed186b4d1b3f58fcf
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-05-10 19:23:46 -0400

    Refactoring Recipe Details
     - Extract the card header and card content into separate components.
     - Use interfaces to define the props for the header and content components.
     - Use a constant for the subheader text instead of a ternary operator.
     - Remove the import of the entire Recipe module, and only import the necessary types.

--- packages/ui/Recipes/Show/Details/Details.tsx
+++ packages/ui/Recipes/Show/Details/Details.tsx
@@ -2,6 +2,42 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@mui/material';
 import * as Recipe from '../../model'
 
+interface RecipeHeaderProps {
+  name: string;
+  isPublic: boolean;
+}
+
+const RecipeHeader: React.FC<RecipeHeaderProps> = ({ name, isPublic }) => {
+  const visibility = isPublic ? 'Public' : 'Private';
+  return <CardHeader title={name} subheader={visibility} />;
+};
+
+interface RecipeContentProps {
+  description: string;
+  preparationTimeSeconds: number;
+  cookingTimeSeconds: number;
+}
+
+const RecipeContent: React.FC<RecipeContentProps> = ({
+  description,
+  preparationTimeSeconds,
+  cookingTimeSeconds,
+}) => {
+  return (
+    <CardContent>
+      <Typography variant="body1" gutterBottom>
+        Description: {description}
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Preparation Time: {preparationTimeSeconds} seconds
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Cooking Time: {cookingTimeSeconds} seconds
+      </Typography>
+    </CardContent>
+  );
+};
+
 const RecipeDetails: React.FC<Recipe.INTERFACE> = ({
   name,
   description,
@@ -11,18 +47,12 @@ const RecipeDetails: React.FC<Recipe.INTERFACE> = ({
 }) => {
   return (
     <Card>
-      <CardHeader title={name} subheader={isPublic ? 'Public' : 'Private'} />
-      <CardContent>
-        <Typography variant="body1" gutterBottom>
-          Description: {description}
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          Preparation Time: {preparationTimeSeconds} seconds
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          Cooking Time: {cookingTimeSeconds} seconds
-        </Typography>
-      </CardContent>
+      <RecipeHeader name={name} isPublic={isPublic} />
+      <RecipeContent
+        description={description}
+        preparationTimeSeconds={preparationTimeSeconds}
+        cookingTimeSeconds={cookingTimeSeconds}
+      />
     </Card>
   );
 };
