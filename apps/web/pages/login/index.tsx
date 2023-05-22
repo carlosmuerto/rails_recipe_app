5b0c6651bac3e57e6c4196908463acfe2aae98a5
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-05-22 19:52:45 -0400

    Fix routing case sentitive

---
+++ apps/web/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import BaseLayout from "ui/Base/Layout";
+import * as Auth from 'ui/Auth';
+
+const LogIn = () => {
+  return (
+    <Auth.Layout>
+      <Auth.LogIn />
+    </Auth.Layout>
+  );
+}
+
+export default LogIn; 
