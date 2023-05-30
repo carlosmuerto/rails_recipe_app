781775df7eb333d51e797412170a28fbbe2197dd
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-05-30 17:22:18 -0400

    LogIn request

--- packages/ui/Auth/LogIn/Form.tsx
+++ packages/ui/Auth/LogIn/Form.tsx
@@ -11,6 +11,8 @@ import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import FormControlLabel from '@mui/material/FormControlLabel'
+import axios from 'axios'
+import { BASE_URL } from '../const'
 
 const AuthLogInFormSchema = object({
   email: string().nonempty('Email is required').email('Email is invalid'),
@@ -41,10 +43,39 @@ const AuthLogInForm = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isSubmitSuccessful])
 
-  const onSubmitHandler: SubmitHandler<AuthLogInFormInput> = (values) => {
-    console.log(values)
+  const onSubmitHandler: SubmitHandler<AuthLogInFormInput> = async (values) => {
+    axios
+      .post(`${BASE_URL}/login`, {
+        user: {
+          email: values.email,
+          password: values.password,
+        },
+      })
+      .then(function (response) {
+        console.log(response)
+      })
+      .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data)
+          console.log(error.response.status)
+          console.log(error.response.headers)
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request)
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message)
+        }
+        console.log(error.config)
+      })
+      .finally(function () {
+        setLoading(false)
+      })
   }
-  // console.log(errors)
 
   return (
     <Fragment>
